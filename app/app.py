8826cfd6f49d9577285e847bfeb6ee36c2912293
kykhoo <kykhoo@YAHOO.COM>
2021-10-16 17:36:18 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def hello_world():
     request_type_str = request.method
-    if request_type_str =='GET':
-        return render_template('index.html',href2='')
+    if request_type_str == 'GET':
+        return render_template('index.html', href2='')
     else:
         num_A = request.form['A']
         num_B = request.form['B']
         final_result = int(num_A) + int(num_B)
-        return render_template('index.html', href2='A= '+str(num_A)+', B= '+str(num_B)+', A+B = '+str(final_result))
+        return render_template('index.html', href2='A = '+str(num_A)+', B= '+str(num_B)+', A + B = '+str(final_result))
       
