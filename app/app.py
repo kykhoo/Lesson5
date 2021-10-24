268b354796696fbd676d0ee3ffa4e76fd542615b
kykhoo <kykhoo@YAHOO.COM>
2021-10-24 09:34:42 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,8 +8,19 @@ def hello_world():
     if request_type_str == 'GET':
         return render_template('index.html', href2='')
     else:
-        num_A = request.form['A']
-        num_B = request.form['B']
-        final_result = int(num_A) + int(num_B)
-        return render_template('index.html', href2='A = '+str(num_A)+', B= '+str(num_B)+', A + B = '+str(final_result))
+        value_soup = 2
+        value_salad = 3
+        value_coffee = 2
+        value_water = 1
+        num_soup = request.form['soup']
+        num_salad = request.form['salad']
+        num_coffee = request.form['coffee']
+        num_water = request.form['water']
+        total_soup = int(num_soup)*value_soup
+        total_salad = int(num_salad)*value_salad
+        total_coffee = int(num_coffee)*value_coffee
+        total_water = int(num_water)*value_water
+        
+        final_result = total_soup + total_salad + total_coffee + total_water 
+        return render_template('index.html', href2='soup='+num_soup+', salad='+num_salad+', coffee='+num_coffee+', water='+num_water+', Total = '+str(final_result))
       
